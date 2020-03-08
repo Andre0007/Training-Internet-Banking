6f79b253c2f36f43da0f73e553be4e41dc9acc0f
André <aa.concha7@gmail.com>
2020-03-08 14:36:32 -0300

    Desenvolvimento do cadastro
    Desenvolvimento do cadastro de usuario em localstorage

--- src/app/cadastro-clientes/cadastro-clientes.component.ts
+++ src/app/cadastro-clientes/cadastro-clientes.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro-clientes',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadastro-clientes.component.css']
 })
 export class CadastroClientesComponent implements OnInit {
+  formCadastro;
+  conversao;
+  valoresForm: Object;
 
-  constructor() { }
+  constructor(private fb: FormBuilder,
+    public dialog: MatDialog,
+    private router: Router) { }
 
   ngOnInit(): void {
+    localStorage.clear();
+    this.formCadastro = this.fb.group({
+      nome: [''],
+      cpf: [''],
+      email: [''],
+      telefone: [''],
+      endereco: ['']
+    });
+    this.formCadastro.valueChanges.pipe(
+      debounceTime(1000))
+      .subscribe(res => {
+        console.log(res);
+        this.valoresForm = res;
+      });
+  }
+
+  cadastro(){
+    console.log(this.formCadastro.controls);
+    this.conversao = JSON.stringify(this.valoresForm);
+    localStorage.setItem('cadastro', this.conversao);
   }
 
 }
